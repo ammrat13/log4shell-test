c6b6450cccbb428edb2d1c5fd94f3c0639f93e11
Ammar Ratnani <ammrat13@gmail.com>
2021-12-30 21:22:04 -0500

    Added notice to RCEMain

--- src/attacker_ldap_registry_setup/codebase/RCEMain.java
+++ src/attacker_ldap_registry_setup/codebase/RCEMain.java
@@ -1,3 +1,11 @@
+// Demonstration main method for remote code execution
+//
+// TWO COPIES OF THIS FILE EXIST:
+// * src/attacker_codebase/src/RCEMain.java
+// * src/attacker_ldap_registry_setup/codebase/RCEMain.java
+// The former is more up to date than the latter and should be trusted in case
+// of discrepancy.
+
 public class RCEMain {
     public static void rceMain() {
         System.out.println("Function rceMain called!");
